541f26cb902d897ed3b68bebf8bfcdd4ab349b0f
Tony Nguyen <nguyenquangtin@gmail.com>
2020-02-21 14:55:37 +0700

    Handle unknown image url

--- movies/src/components/MovieListItem.vue
+++ movies/src/components/MovieListItem.vue
@@ -1,19 +1,26 @@
 <template>
   <li>
-    <img :src="movie.Poster"/>
-      <a :href="`https://www.imdb.com/title/${movie.imdbID}`" target="_blank">More</a>
+    <img :src="imageURL"/>
+    <a :href="`https://www.imdb.com/title/${movie.imdbID}`" target="_blank">More</a>
     <p>{{ movie.Title }}</p>
     <span>{{ movie.Year }}</span>
   </li>
 </template>
 
 <script>
+import unknownImage from '@/assets/unknown.jpg'
+
 export default {
   name: 'movie-list-item',
   props: {
     movie: {
       type: Object,
       required: true
+    }
+  },
+  computed: {
+    imageURL () {
+      return this.movie.Poster === 'N/A' ? unknownImage : this.movie.Poster
     }
   }
 }
